8204e82f5b54acd31e3e210f8514c1add11241ec
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-05-21 21:08:53 -0300

    Creating Banner

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import { Route, Routes, useLocation } from "react-router-dom";
 
 const App = () => {
-  return <div>
-    <NavBar />
-  </div>;
+  const isSallerPath = useLocation().pathname.includes("seller");;
+
+  return (
+    <div>
+      {isSallerPath ? null : <NavBar />}
+
+      <div className={`${isSallerPath} ? '' : "px-6 md:px-16 lg:px-24 xl:px-32"`}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
-export default App
+export default App;
