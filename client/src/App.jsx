543e20afa2bc3b8a7ece2f55f621957706da5c04
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-05-21 22:16:05 -0300

    Create Categories List

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
     <div>
       {isSallerPath ? null : <NavBar />}
 
-      <div className={`${isSallerPath} ? '' : "px-6 md:px-16 lg:px-24 xl:px-32"`}>
+      <div className={isSallerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
