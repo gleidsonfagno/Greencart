2486565781ca4d204d5d8efdac4a2e2568c0819a
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-05-23 21:20:28 -0300

    Create component Bestsellers

---
+++ client/src/components/BestSellers.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ProductCard from "./ProductCart";
+import { useAppContext } from "../context/AppContext";
+
+function BestSellers() {
+  const { products } = useAppContext();
+  return (
+    <div className="mt-16">
+      <p className="text-2xl md:text-3xl font-medium">Best Sellers</p>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6">
+        {products
+          .filter((products) => products.inStock)
+          .slice(0, 5)
+          .map((products, index) => (
+            <ProductCard key={index} product={products} />
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default BestSellers;
